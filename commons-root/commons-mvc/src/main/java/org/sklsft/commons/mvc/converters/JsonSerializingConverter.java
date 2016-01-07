0072d1624de942bc756481f749ca0584fbd82045
nthibault <nicolas.thibault@ymail.com>
2016-01-07 23:32:48 +0100

    view scope converter
    Conflicts:
    	commons-root/commons-api/pom.xml
    	commons-root/commons-crypto/pom.xml
    	commons-root/commons-mapper/pom.xml
    	commons-root/commons-mvc/pom.xml
    	commons-root/commons-rest-client/pom.xml
    	commons-root/commons-rest-security/pom.xml
    	commons-root/commons-rest/pom.xml
    	commons-root/pom.xml

---
+++ commons-root/commons-mvc/src/main/java/org/sklsft/commons/mvc/converters/JsonSerializingConverter.java
@@ -0,0 +1,40 @@
+package org.sklsft.commons.mvc.converters;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+import org.apache.commons.codec.binary.Base64;
+import org.sklsft.commons.crypto.serialization.JsonSerializer;
+
+public class JsonSerializingConverter<T> implements Converter {
+	
+	private JsonSerializer serializer;
+	private Class<T> clazz;
+	
+	public JsonSerializingConverter(Class<T> clazz, JsonSerializer serializer) {
+		super();
+		this.clazz = clazz;
+		this.serializer = serializer;
+	}
+	
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		try {
+			return serializer.deserialize(new String(Base64.decodeBase64(value)), clazz);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		try {
+			return Base64.encodeBase64URLSafeString(serializer.serialize(value).getBytes());
+		} catch (IOException e) {
+			return null;
+		}
+	}
+}
