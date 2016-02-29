228fe30c1efed1b3d98461211c6c38a795879d31
supralog-nthi <nthibault@supralog.com>
2016-02-29 18:32:38 +0100

    string to obj converter

--- commons-root/commons-mapper/src/main/java/org/sklsft/commons/mapper/impl/StringArrayToBeanMapperImpl.java
+++ commons-root/commons-mapper/src/main/java/org/sklsft/commons/mapper/impl/StringArrayToBeanMapperImpl.java
@@ -1,10 +1,5 @@
 package org.sklsft.commons.mapper.impl;
 
-import java.lang.reflect.Field;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.sklsft.commons.mapper.beans.AccessibleField;
 import org.sklsft.commons.mapper.beans.MappableBean;
 import org.sklsft.commons.mapper.beans.MappableBeanFactory;
@@ -13,7 +8,6 @@ import org.sklsft.commons.mapper.interfaces.ObjectArrayToBeanMapper;
 public class StringArrayToBeanMapperImpl<T> implements ObjectArrayToBeanMapper<T> {
 	
 	private final MappableBean<T> mappableBean;
-	private final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 	
 	public StringArrayToBeanMapperImpl (Class<T> clazz) {
 		mappableBean = MappableBeanFactory.getMappableBean(clazz);
@@ -24,7 +18,7 @@ public class StringArrayToBeanMapperImpl<T> implements ObjectArrayToBeanMapper<T
 		
 		for (int i = 0;i<stringArray.length;i++) {
 			AccessibleField accessibleField = mappableBean.accessibleFields.get(i + startField);
-			mappableBean.setValue(accessibleField.field.getName(), getObjectFromString((String)stringArray[i], accessibleField.field), obj);
+			mappableBean.setValue(accessibleField.field.getName(), StringToObjectConverter.getObjectFromString((String)stringArray[i], accessibleField.field.getType()), obj);
 		}
 		
 		return obj;
@@ -36,34 +30,5 @@ public class StringArrayToBeanMapperImpl<T> implements ObjectArrayToBeanMapper<T
 		return mapFrom(obj, objectArray, 0);
 	}
 	
-	private Object getObjectFromString(String value, Field field) {
-		
-		if (value.equals("")) {
-			return null;
-		}
-
-		if (field.getType().equals(Date.class)) {
-			try {
-				Date date = format.parse(value);
-				return date;
-			} catch (ParseException e) {
-				throw new IllegalArgumentException("Invalid string representation of a date", e);
-			}
-		}
-
-		if (field.getType().equals(Double.class)) {
-			return Double.valueOf(value);
-		}
-
-		if (field.getType().equals(Long.class)) {
-			return Long.valueOf(value);
-		}
-
-		if (field.getType().equals(Boolean.class)) {
-                return Boolean.valueOf(value);
-		}
-		
-		return value;
-
-	}
+	
 }
