21c10aa32741b6d191b32cac1b649033ac86e0ef
supralog-nthi <nthibault@supralog.com>
2016-03-10 19:12:18 +0100

    ajax method template

---
+++ commons-root/commons-mvc/src/main/java/org/sklsft/commons/mvc/ajax/AjaxMethodTemplate.java
@@ -0,0 +1,19 @@
+package org.sklsft.commons.mvc.ajax;
+
+
+/**
+ * This interface must be used in combination to the {@link AjaxMethodExecutor}<br>
+ * It simply templatize the proceeding of a ajax method call :
+ * <li> a service execution that can produce a result
+ * <li> a redirection that can depend on the result of the execution
+ * 
+ * @author Nicolas Thibault
+ *
+ */
+public interface AjaxMethodTemplate {
+	
+	Object execute();
+	
+	void redirectOnComplete(Object result);
+
+}
