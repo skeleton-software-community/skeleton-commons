412e767b59c9e279b8d0dbf4a852caabf4b74f2f
supralog-arup <arupp@supralog.com>
2016-03-18 15:43:12 +0100

    Add compareTo, Equals to SelectItem and create SelectItemConverter

--- commons-root/commons-api/src/main/java/org/sklsft/commons/api/model/SelectItem.java
+++ commons-root/commons-api/src/main/java/org/sklsft/commons/api/model/SelectItem.java
@@ -2,7 +2,13 @@ package org.sklsft.commons.api.model;
 
 import java.io.Serializable;
 
-public class SelectItem implements Serializable {
+
+/**
+ * Tuple to be used for instance in JSF dropdown options
+ * @author Alexandre RUPP
+ *
+ */
+public class SelectItem implements Serializable, Comparable<SelectItem> {
 
 	private static final long serialVersionUID = 1L;
 	
@@ -42,4 +48,51 @@ public class SelectItem implements Serializable {
 	public void setLabel(String label) {
 		this.label = label;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((label == null) ? 0 : label.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SelectItem other = (SelectItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (label == null) {
+			if (other.label != null)
+				return false;
+		} else if (!label.equals(other.label))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(SelectItem selectItem) {
+		if(this.label == null)
+			if(selectItem.getLabel() == null)
+				return 0;
+			else
+				return -1;
+		else 
+			if(selectItem.getLabel() == null)
+				return 1; 
+			else
+				return this.label.compareTo(selectItem.getLabel());
+	}
+	
+	
 }
