28ef953dfd4e1d78e5ac95fc09e5ed549ff0671f
nthibault <nicolas.thibault@ymail.com>
2016-06-17 20:59:44 +0200

    correction sur les mappers : Les methodes setValue et getValue sont liées à l'accessibleField !

--- commons-root/commons-mapper/src/main/java/org/sklsft/commons/mapper/beans/MappableBean.java
+++ commons-root/commons-mapper/src/main/java/org/sklsft/commons/mapper/beans/MappableBean.java
@@ -1,7 +1,5 @@
 package org.sklsft.commons.mapper.beans;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
 
@@ -13,35 +11,4 @@ public class MappableBean<T> {
 	
 	public Map<String,AccessibleField> accessibleFieldsMap;
 	
-	public Object getValue(String fieldName, T object) {
-		
-		AccessibleField accessibleField = accessibleFieldsMap.get(fieldName);
-		if (accessibleField == null) {
-			throw new IllegalArgumentException("No accessible field " + fieldName + " for class " + beanClass.getName());
-		}
-		
-		Method getter = accessibleField.getter;
-		
-		try {
-			return getter.invoke(object);
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
-	
-	public void setValue(String fieldName, Object fieldValue, T object) {
-		
-		AccessibleField accessibleField = accessibleFieldsMap.get(fieldName);
-		if (accessibleField == null) {
-			throw new IllegalArgumentException("No accessible field " + fieldName + " for class " + beanClass.getName());
-		}
-		
-		Method setter = accessibleField.setter;
-		
-		try {
-			setter.invoke(object, fieldValue);
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
 }
