2d7b50f65c47cd3bb7e7970d3ff61462491e0772
supralog-nthi <nthibault@supralog.com>
2016-09-22 12:53:41 +0200

    correction exceptions et logging aspect

--- commons-root/commons-rest/src/main/java/org/sklsft/commons/rest/exception/RestExceptionHandler.java
+++ commons-root/commons-rest/src/main/java/org/sklsft/commons/rest/exception/RestExceptionHandler.java
@@ -25,6 +25,8 @@ public class RestExceptionHandler {
 	@ExceptionHandler(ApplicationException.class)
 	public @ResponseBody ErrorReport handleApplicationException(ApplicationException e) {
 		
+		logger.error("exception thrown : " + e.getMessage(), e);
+		
 		ErrorReport errorReport = new ErrorReport();
 		errorReport.setExceptionClassName(e.getClass().getName());
 		errorReport.setMessage(e.getMessage());
@@ -35,6 +37,8 @@ public class RestExceptionHandler {
 	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(Exception.class)
 	public @ResponseBody ErrorReport handleException(Exception e) {
+		
+		logger.error("exception thrown : " + e.getMessage(), e);
 		
 		ErrorReport errorReport = new ErrorReport();
 		errorReport.setExceptionClassName(TechnicalError.class.getName());
