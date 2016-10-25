88059fd27134620bb84e4ffd43e5d96084192f7b
supralog-nthi <nthibault@supralog.com>
2016-10-25 17:08:39 +0200

    AbstractMapper to avoid implementing mapFrom(U) and mapTo(T)

--- commons-root/commons-mapper/src/main/java/org/sklsft/commons/mapper/interfaces/Mapper.java
+++ commons-root/commons-mapper/src/main/java/org/sklsft/commons/mapper/interfaces/Mapper.java
@@ -1,5 +1,12 @@
 package org.sklsft.commons.mapper.interfaces;
 
+/**
+ * A simple interface to define mapping standard functions
+ * @author Nicolas Thibault
+ *
+ * @param <T>
+ * @param <U>
+ */
 public interface Mapper<T, U> {
 	
 	T mapFrom (T obj1, U obj2);
